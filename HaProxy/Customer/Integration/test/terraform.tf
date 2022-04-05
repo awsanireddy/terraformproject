57c09b8ed8d9b4adfe7a01056841b888982576d5
awsanireddy <44488232+awsanireddy@users.noreply.github.com>
2022-04-05 16:29:17 -0400

    Delete HaProxy directory

--- HaProxy/Customer/Integration/test/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "integration-test-stack"
-    key     = "state/test.tfstate"
-    region  = "us-east-2"
-    profile = "integration"
-  }
-}
--- HaProxy/Customer/TEST/test/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "tha-mirth-prod-terraform"
-    key     = "state/haproxy.tfstate"
-    region  = "us-east-1"
-    profile = "tha"
-  }
-}
